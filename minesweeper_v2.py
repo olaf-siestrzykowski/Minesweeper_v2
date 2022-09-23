bad332edb0f0ce36ad1a0f0204b5ecf5083f9d27
olfis <olfis@o2.pl>
2022-09-23 12:48:33 +0200

    restarting to ad

--- minesweeper_v2.py
+++ minesweeper_v2.py
@@ -3,38 +3,60 @@ from tkinter import *
 import random
 import ctypes
 import sys
+from PIL import Image, ImageTk
 
 size = 20
 bombs_count = 75
+sys.setrecursionlimit(10000)
 
 
 class Cells():
     all = []
     cell_count = size**2-bombs_count
+    bomb_count = bombs_count
     cell_count_label = None
+    bomb_count_label = None
 
     def __init__(self, x, y, is_bomb=False):
         self.is_bomb = is_bomb
         self.is_opened = False
+        self.is_flagged = False
         self.cell_button_object = None
         self.x = x
         self.y = y
+        self.bombs_around = 0
         Cells.all.append(self)
 
     def left_click(self, event):
         if self.is_bomb:
+            self.cell_button_object.configure(bg="red") #not working
             ctypes.windll.user32.MessageBoxW(0, "You clicked on a mine", "Game Over", 0)
             sys.exit()
         else:
+            self.show_cell()
             if self.show_cell() == "":
                 for cell_obj in self.surrounded_cells():
                     cell_obj.show_cell()
                     cell_obj.cell_button_object.unbind("<Button-3>")
-            self.show_cell()
         self.cell_button_object.unbind("<Button-3>")
 
     def right_click(self, event):
-        self.cell_button_object.configure(bg="orange")
+
+        if self.is_flagged:
+            self.is_flagged = False
+            self.cell_button_object.configure(bg="SystemButtonFace", image="")
+            self.cell_button_object.bind("<Button-1>", self.left_click)
+            Cells.bomb_count += 1
+            if Cells.bomb_count_label:
+                Cells.bomb_count_label.configure(text=f"Bombs left:{Cells.bomb_count}")
+
+        elif not self.is_flagged:
+            self.is_flagged = True
+            self.cell_button_object.configure(bg='orange')
+            self.cell_button_object.unbind("<Button-1>")
+            Cells.bomb_count -= 1
+            if Cells.bomb_count_label:
+                Cells.bomb_count_label.configure(text=f"Bombs left:{Cells.bomb_count}")
 
     def create_button_object(self, location):
         button = Button(location, text="", width=int(100/size), height=int(50/size))
@@ -47,8 +69,13 @@ class Cells():
                       height=8, bg="black", fg="white", font=("consolas", 18))
         Cells.cell_count_label = label
 
+    def create_bomb_count(self):
+        label = Label(self, text=f"Bombs left:{Cells.bomb_count}", width=16,
+                      height=8, bg="black", fg="white", font=("consolas", 18))
+        Cells.bomb_count_label = label
+
     def randomize_bombs():
-        bomb_cells = random.sample(Cells.all,bombs_count)
+        bomb_cells = random.sample(Cells.all, bombs_count)
         for bomb_cell in bomb_cells:
             bomb_cell.is_bomb = True
 
@@ -69,25 +96,37 @@ class Cells():
         cells = [cell for cell in cells_around if cell is not None]
         return cells
 
-    def show_cell(self):
-        bomb = 0
-        if not self.is_opened:
-            Cells.cell_count -= 1
+    def bomb_counter(self):
         for cell in self.surrounded_cells():
             if cell.is_bomb:
-                bomb += 1
+                self.bombs_around += 1
+        return self.bombs_around
+
+    def show_cell(self):
+        self.bombs_around = 0
+
+        if not self.is_opened:
+            Cells.cell_count -= 1
+        self.bomb_counter()
         self.is_opened = True
-        if bomb == 0:
-            bomb = ""
+
+        if self.bombs_around == 0:
+            self.bombs_around = ""
+            for empty_cell in self.surrounded_cells():
+                if empty_cell.bombs_around == 0:
+                    empty_cell.show_cell()
+                    empty_cell.cell_button_object.unbind("<Button-3>")
+
         if Cells.cell_count_label:
             Cells.cell_count_label.configure(text=f"Cells left:{Cells.cell_count}")
+
         if Cells.cell_count == 0:
             Cells.cell_count_label.configure(text=f"Cells left:{Cells.cell_count}\n"
                                                   f"YOU WIN!!!")
             ctypes.windll.user32.MessageBoxW(0, "Congratulations :)", "You win!", 0)
 
-        self.cell_button_object.configure(text=str(bomb), bg="white")
-        return bomb
+        self.cell_button_object.configure(text=str(self.bombs_around), bg="white")
+        return self.bombs_around
 
     def __repr__(self):
         return f"Cell({self.x}, {self.y})"
@@ -107,21 +146,22 @@ game_title = Label(top_frame, bg="#460000", fg="white", text=" Minesweeper Game"
 game_title.grid(row=0, column=3)
 game_board = Frame(window)
 
+
 score_frame = Frame(window, bg="#FF7A2F", width=200, height=(HEIGHT-100))
 score_frame.place(x=0, y=120)
 
-#for i in range(bombs_count):
-
 for row in range(size):
     for column in range(size):
-        button = Cells(row,column)
+        button = Cells(row, column)
         button.create_button_object(game_board)
         button.cell_button_object.grid(row=row, column=column)
 
 game_board.place(x=200, y=120)
 
 Cells.create_cell_count(score_frame)
+Cells.create_bomb_count(score_frame)
 Cells.cell_count_label.place(x=0, y=0)
+Cells.bomb_count_label.place(x=0, y=200)
 
 Cells.randomize_bombs()
 
